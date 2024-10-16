8bcab4e6b78eedac9fd0a0a4593ad40c1c4e4f9f
Guy <ga327@sussex.ac.uk>
2024-10-16 04:44:28 +0100

    Solution to problem #6: Zigzag Conversion

---
+++ p6.erl
@@ -0,0 +1,20 @@
+-module(p6).
+-export([convert/2]).
+
+-spec convert(S :: unicode:unicode_binary(), NumRows :: integer()) -> unicode:unicode_binary().
+
+convert(S, 1) -> S;
+convert(S, NumRows) ->
+    convert(S, [{I,<<>>} || I <- lists:seq(1,NumRows)], []).
+
+convert(<<>>, [], PrevRows = [{1,_}|_]) ->
+    << T || {_,T} <- PrevRows>>;
+convert(<<>>, [NextRow|NextRows], PrevRows) ->
+    convert(<<>>, NextRows, [NextRow|PrevRows]);
+convert(S, [], [PrevRow|PrevRows]) ->
+    convert(S, PrevRows, [PrevRow]);
+convert(S, NextRows, PrevRows) ->
+    <<FirstChar:8, NewS/binary>> = S,
+    [{I,CurrentRow} | NewNextRows] = NextRows,
+    NewRow = {I, <<CurrentRow/binary, FirstChar:8>>},
+    convert(NewS, NewNextRows, [NewRow | PrevRows]).
