610a1881aa67209ea9d220dad2716eb3ca05950e
Guy <ga327@sussex.ac.uk>
2024-10-16 05:28:02 +0100

    Solution to problem #150: Evaluate Reverse Polish Notation

---
+++ p150.erl
@@ -0,0 +1,17 @@
+-module(p150).
+-export([eval_rpn/1]).
+
+-spec eval_rpn(Tokens :: [unicode:unicode_binary()]) -> integer().
+
+eval_rpn(Tokens) -> eval_rpn(Tokens, []).
+
+eval_rpn([], Stack) -> hd(Stack);
+eval_rpn([Token|RemTokens], Stack) ->
+    NewStack = case Token of
+        <<"+">> -> [A,B|C] = Stack, [B+A|C];
+        <<"-">> -> [A,B|C] = Stack, [B-A|C];
+        <<"*">> -> [A,B|C] = Stack, [B*A|C];
+        <<"/">> -> [A,B|C] = Stack, [B div A|C];
+        N -> [binary_to_integer(N)|Stack]
+        end,
+    eval_rpn(RemTokens, NewStack).
