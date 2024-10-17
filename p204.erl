270a65eeca0996022bb295edf5e28c01b1fb7c6b
Guy <ga327@sussex.ac.uk>
2024-10-17 06:03:38 +0100

    Lessen reliance on queue for p204

--- p204.erl
+++ p204.erl
@@ -1,33 +1,29 @@
 % Too slow
 
--module(p204).
+-module(p204_2).
 -export([count_primes/1]).
 
 -spec count_primes(N :: integer()) -> integer().
 
-count_primes(N) -> count_primes(N, queue:from_list([2]), 1).
-count_primes(N, KnownPrimes, PrimeCount) ->
-    P = queue:get_r(KnownPrimes),
-    case P >= N of
+count_primes(N) -> count_primes(N, queue:from_list([2]), 1, 2).
+count_primes(N, KnownPrimes, PrimeCount, LargestPrime) ->
+    case LargestPrime >= N of
        true  -> PrimeCount - 1;
        false ->
-            NewPrimes = add_prime(KnownPrimes),
-            count_primes(N, NewPrimes, PrimeCount+1)
+            {NewLargestPrime, NewPrimes} = add_prime(KnownPrimes, LargestPrime + 1),
+            count_primes(N, NewPrimes, PrimeCount+1, NewLargestPrime)
     end.
 
-add_prime(KnownPrimes) -> add_prime(KnownPrimes,queue:get_r(KnownPrimes)+1).
 add_prime(KnownPrimes, N) ->
     case is_prime(N, KnownPrimes) of
-        true  -> queue:in(N, KnownPrimes);
+        true  -> {N, queue:in(N, KnownPrimes)};
         false -> add_prime(KnownPrimes, N+1)
     end.
 
 is_prime(N, PrimeQueue) ->
     {{value,Prime}, RemainingPrimes} = queue:out(PrimeQueue),
-    case N < Prime*Prime of
-        true -> true;
-        _ -> case N rem Prime of
-                0 -> false;
-                _ -> is_prime(N, RemainingPrimes)
-             end
+    case {N<Prime*Prime, N rem Prime} of
+        {true,_} -> true;
+        {_, 0}   -> false;
+        {_, _}   -> is_prime(N, RemainingPrimes)
     end.
